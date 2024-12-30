35a1ce4a45255b4e3e7d48aaa73b2182eb527b33
Ouw0pp <54865984+11Ouw0pp11@users.noreply.github.com>
2024-12-29 22:09:39 -0600

    Remove OuwUI directory from tracking

--- src/ReplicatedStorage/OuwUI/Instances/CompileProperties/Bases/ImageButton.lua
+++
@@ -1,6 +0,0 @@
-return {
-	BackgroundColor3 = Color3.new(1,1,1);
-	BorderSizePixel = 0;
-	Size = UDim2.fromOffset(100,100);
-	Image = "";
-}
--- src/ReplicatedStorage/OuwUI/Utility/Menum.lua
+++
@@ -1,5 +0,0 @@
-return {
-	ListenerType = {
-		ApplyProperty = "ApplyProperty";
-	};
-}
