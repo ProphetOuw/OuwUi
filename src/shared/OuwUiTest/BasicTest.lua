851424905f5448170dff0d0bf8f1dded0d04fa5c
Ouw0pp <54865984+11Ouw0pp11@users.noreply.github.com>
2024-12-30 19:03:40 -0600

    wally setup

---
+++ src/shared/OuwUiTest/BasicTest.lua
@@ -0,0 +1,10 @@
+game.ReplicatedStorage:WaitForChild("Shared")
+game.ReplicatedStorage.Shared:WaitForChild("OuwUI")
+local OuwUi = require(game.ReplicatedStorage.Shared.OuwUI)
+return function()
+
+
+    return function()
+        
+    end
+end
